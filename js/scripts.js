95a62b690d806ddfc9e9571d3d5c0600df8c889c
Joanna <piotrowskaWIMIIP@gmail.com>
2018-04-08 23:38:30 +0200

    Add the code to the JS file with the whole Christmas tree

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,36 @@
+// half christmasTree
 function drawTree(heightTree) {
-  for (var i = 1 ; i <= heightTree ; i++) {
+  for (var i = 0 ; i <= heightTree ; i++) {
     var star = " ";
     for (var j = 1 ; j <= i ; j++) {
       star += '*';
     }
     console.log(star);
     document.write("<br>"+star);
+  }
+};
+
+var heightTree = prompt("Please enter height your Christmastree");
+drawTree(heightTree);
+
+// all christmasTree
+function drawTree(heightTree) {
+  for (var iterator = 0 ; iterator < heightTree ; iterator++) {
+    // spaces
+    for (var amountSpace = heightTree-1 ; amountSpace >= iterator ; amountSpace--) {
+      space = "&nbsp&nbsp";
+      console.log(space);
+      document.write(space);
+    }
+    // starts
+    for (var amountStar = 0 ; amountStar <= iterator*2 ; amountStar++) {
+      star = "*";
+      console.log(star);
+      document.write(star);
+    }
+    // new line
+    console.log("<br>");
+    document.write("<br>");
   }
 };
 
