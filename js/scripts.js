7aaa381d3a0b24ba9ca08633e13ca3e2bb9effc8
Joanna <piotrowskaWIMIIP@gmail.com>
2018-04-09 15:00:11 +0200

    Add a new way to create a tree in the JS file

--- js/scripts.js
+++ js/scripts.js
@@ -13,24 +13,41 @@ function drawTree(heightTree) {
 var heightTree = prompt("Please enter height your Christmastree");
 drawTree(heightTree);
 
-// all christmasTree
+// all christmasTree - the first way
 function drawTree(heightTree) {
   for (var iterator = 0 ; iterator < heightTree ; iterator++) {
     // spaces
     for (var amountSpace = heightTree-1 ; amountSpace >= iterator ; amountSpace--) {
       space = "&nbsp&nbsp";
       console.log(space);
-      document.write(space);
+      // document.write(space);
     }
     // starts
-    for (var amountStar = 0 ; amountStar <= iterator*2 ; amountStar++) {
+    for (var amountStar = 0 ; amountStar <= (iterator*2 )-1 ; amountStar++) {
       star = "*";
       console.log(star);
-      document.write(star);
+      // document.write(star);
     }
     // new line
     console.log("<br>");
-    document.write("<br>");
+    // document.write("<br>");
+  }
+};
+
+// all christmasTree- the second way (only one variable)
+var heightTree = prompt("Please enter height your Christmastree");
+drawTree(heightTree);
+
+function drawTree(heightTree) {
+  for (iterator = 0 ; iterator <= heightTree ; iterator++) {
+    tree = "";
+    for (amountSpace = iterator ; amountSpace < heightTree ; amountSpace++) {
+      tree += " ";
+    }
+    for (amountStar = 0 ; amountStar < (iterator*2)-1 ; amountStar++) {
+      tree += "*";
+    }
+    console.log(tree);
   }
 };
 
